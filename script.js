30e7e15f2477dcf998785695f8adb5c38d578d25
anjhen <anjhen.villanueva1@gmail.com>
2024-06-01 20:13:22 +0800

    updated portfolio

---
+++ script.js
@@ -0,0 +1,20 @@
+
+    function showIframe() {
+        document.getElementById('iframeContainer').style.display = 'block';
+        document.getElementById('overlay').style.display = 'block';
+    }
+
+    function closeIframe() {
+        document.getElementById('iframeContainer').style.display = 'none';
+        document.getElementById('overlay').style.display = 'none';
+    }
+
+    function closeIframe() {
+    var iframe = document.querySelector('#iframeContainer iframe');
+    var iframeSrc = iframe.src;
+    iframe.src = '';  
+    iframe.src = iframeSrc; 
+    document.getElementById('iframeContainer').style.display = 'none';
+    document.getElementById('overlay').style.display = 'none';
+}
+
